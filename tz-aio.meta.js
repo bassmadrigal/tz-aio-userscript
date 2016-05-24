0a818b24a85b7785f42a1b7a5ddb036e98fb5b88
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2016-05-23 23:54:06 -0400

    Update tz-aio.meta.js
    Updated downloadURL, updateURL for new repository. Also incremented version, date in line with the main script.

--- tz-aio.meta.js
+++ tz-aio.meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name          Torrentz All-in-One
-// @version       2.9.8
-// @date          2015-09-28
+// @version       2.9.10
+// @date          2016-05-23
 // @namespace     http://elundmark.se/code/tz-aio/
-// @homepage      https://github.com/elundmark/tz-aio-userscript
-// @downloadURL   http://f.3r1k.se/js/tz-aio/tz-aio.user.js
-// @updateURL   http://f.3r1k.se/js/tz-aio/tz-aio.meta.js
+// @homepage      https://github.com/bassmadrigal/tz-aio-userscript
+// @downloadURL   https://github.com/bassmadrigal/tz-aio-userscript/raw/master/tz-aio.user.js
+// @updateURL     https://github.com/bassmadrigal/tz-aio-userscript/raw/master/tz-aio.meta.js
 // ==/UserScript==

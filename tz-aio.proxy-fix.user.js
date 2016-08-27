83847360ab14ea6b97026af127fbaaaaea18855e
bassmadrigal <jebrhansen+SBo@gmail.com>
2016-08-27 12:19:29 -0400

    Add "noopener" to rel attribute to fix vulnerability
    See the following link for more details.
    https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/

--- tz-aio.proxy-fix.user.js
+++ tz-aio.proxy-fix.user.js
@@ -1927,7 +1927,7 @@ if (!String.prototype.trim) {
 					"href": __.unescape(getNoReferrerUrl(__.unescape(arr[1]).replace(/%s/g, encc(str)))),
 					"class": "search_link",
 					"attr": {
-						"rel": "noreferrer",
+						"rel": "noreferrer noopener",
 						"target": "_blank"
 					}
 				})

5038b555128e265d29e6899141dbac2d3d6f2472
bassmadrigal <jebrhansen+SBo@gmail.com>
2016-09-04 17:16:01 -0400

    Updated meta for new release to allow automatic updates

--- tz-aio.meta.js
+++ tz-aio.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Torrentz All-in-One
-// @version       2.9.12
-// @date          2016-08-27
+// @version       2.9.13
+// @date          2016-09-04
 // @namespace     http://elundmark.se/code/tz-aio/
 // @homepage      https://github.com/bassmadrigal/tz-aio-userscript
 // @downloadURL   https://github.com/bassmadrigal/tz-aio-userscript/raw/master/tz-aio.user.js
